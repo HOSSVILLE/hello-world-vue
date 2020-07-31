f3d096199499393af1e701fac41971be94077384
Carl <carl.hostrander00@gmail.com>
2020-07-30 21:19:18 -0500

    adding another section

---
+++ src/views/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/MULOGO.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+   <label for="firstName">First Name: </label>
+   <input placeholder="Type here" type="text" v-model="firstName" id="firstName">
+  </div>
+  </div>
+ </template>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
